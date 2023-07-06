59fedfe1e0dd3acd5dcd950f7e02d8c465718f4f
Clement Stutz <clement.stutz@cea.fr>
2023-07-06 15:42:52 +0200

    maj script/setup.py

--- setup.py
+++ setup.py
@@ -2,39 +2,52 @@
 
 """Installation script of the package."""
 
-import os
+import pathlib
 from setuptools import find_packages, setup
+
+
+here = pathlib.Path(__file__).parent.resolve()
+
 
 def get_version():
     """Extract the package's version number from the ``VERSION`` file."""
-    filename = os.path.join(os.path.dirname(__file__), "src", "uranie_launcher", "VERSION")
-    with open(filename, encoding = 'utf-8') as file:
-        return file.read().strip()
+    return (here / "src" / "uranie_launcher" / "VERSION").read_text(encoding="utf-8").strip()
+
+
+def get_long_description():
+    """Extract README content"""
+    return (here / "README.md").read_text(encoding="utf-8")
 
 
 setup(
-    name = "uranie-launcher",
-    version = get_version(),
-    author = "Clément STUTZ",
-    author_email = "clement.stutz@cea.fr",
-    description = "Uranie launcher for your own calculation scripts",
-    package_dir = {"": "src"},
-    packages = find_packages(where = "src"),
-    entry_points = {
+    name="uranie-launcher",
+    version=get_version(),
+    author="Clément STUTZ",
+    author_email="clement.stutz@cea.fr",
+    description="Uranie launcher for your own calculation scripts",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    entry_points={
         'console_scripts': [
             'uranie-launcher-unitary=uranie_launcher._run_unitary:run_unitary',
             'test-run-unitary-calculation=tests.program_tester:run_unitary_calculation',
         ]},
-    install_requires = ["numpy>=1.19.5",
+    install_requires=["numpy>=1.19.5",
                     #   "URANIE",
                     #   "ROOT"
                       ],
-    extras_require = {
+    extras_require={
             'dev' : ["pytest"]
         },
     package_data={
         "uranie_launcher": [
             "VERSION",
         ],
+    },
+    project_urls={  # Optional
+        "Bug Reports": "https://github.com/clementstutz/uranie-launcher/issues",
+        "Source": "https://github.com/clementstutz/uranie-launcher",
     },
     )
